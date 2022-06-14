2306a8b76eb9dc6832fae2d018735ad24c750d5e
Sar <samuel.alonso@people-media.com.mx>
2022-06-14 12:35:25 -0500

    se termino de animar el airon y cap

--- main.js
+++ main.js
@@ -11,8 +11,8 @@ window.onload = function(){
     lax.addElements('.logo',{
         scrollY:{
             scale:[
-                [0,80],
-                [1,3]
+                [0,100],
+                [.5,3],
             ],
         opacity:[
             [0,70],
@@ -57,11 +57,11 @@ window.onload = function(){
     lax.addElements('.thanos',{
         scrollY:{
         opacity:[
-            [50,120],
+            [70,120],
             [0,1]
         ],
         translateY:[
-            [0,400],
+            [0,800],
             [0,80]
         ],
 
@@ -75,7 +75,7 @@ window.onload = function(){
             [1,0]
         ],
         translateY:[
-            [0,800],
+            [0,900],
             [-250,0]
         ],
 
@@ -93,15 +93,153 @@ window.onload = function(){
     lax.addElements('.ironman-title',{
         scrollY:{
         filter:[
-            [600,700,800,900,1000],
-            [0,50,0,50,50],
+            [400,500,600,700,800,900,1000,1100],
+            [0,50,0,50,0,50,0,50],
             {
                 cssFn: function(value){
                     return `drop-shadow(0 0 ${value}px yellow)`;
                 },
             },
         ],
+        translateY:[
+            [600,1400], 
+            [90,"elCenterY"],
+        ]
+
        },
     });
 
+    lax.addElements('.ironman-01',{
+        scrollY:{
+         translateY:[
+            [300,1500],
+            [100,-1000],
+        ]
+
+       },
+    });
+
+
+    lax.addElements('.ironman-02',{
+        scrollY:{
+            scale:[
+                [1200,2500],
+                [0,2],
+            ],
+         translateX:[
+            [1200,1350,1900],
+            [0,500,-700],
+        ],
+
+        translateY:[
+            [1200,1350,1900],
+            [-600,-400,-300],
+        ],
+       },
+    });
+
+
+    lax.addElements('.shield',{
+        scrollY:{
+        translateX:[
+            [1900,2100,2200,2300],
+            [-800,"screenWidth-200",0,"screenWidth+200"],
+            
+        ],
+        translateY:[
+            [1900,2100,2200,2300],
+            [-100,50,200,400],
+        ],
+
+       },
+    });
+
+    lax.addElements('.capital-title',{
+        scrollY:{
+        filter:[
+            [1900,2000,2100,2200,2300,2400,2500,2600],
+            [0,50,0,50,0,50,0,50],
+            {
+                cssFn: function(value){
+                    return `drop-shadow(0 0 ${value}px blue)`;
+                },
+            },
+        ],
+        translateY:[
+            [1900,2100],
+            [-400,"elCenterY-2600"],
+        ],
+
+       },
+    });
+   
+
+    lax.addElements('.cap2',{
+        scrollY:{
+        translateX:[
+                [2400,2800],
+                [200,-800],
+                
+            ],
+       translateY:[
+            [2400,2800],
+            [-400,-400],
+        ],
+        scale:[
+            [2400,2800],
+            [1,0.1],
+        ],
+        
+        opacity:[
+            [2400,2800],
+            [0,3],
+        ]
+
+       },
+    });
+
+    lax.addElements('.shield02',{
+        scrollY:{
+        translateX:[
+            [2400,2800],
+            [260,"screenWidth+1900"],
+            
+        ],
+        translateY:[
+            [2400,2800],
+            [-1000,-1000],
+        ],
+        opacity:[
+            [2400,2800],
+            [0,3],
+        ],
+        scale:[
+            [2400,2800],
+            [1,4],
+        ],
+
+       },
+    });
+
+    lax.addElements('.rayo',{
+        scrollY:{
+        scaleX:[
+            [2800,3000],
+            [0,1],
+            
+        ],
+        translateY:[
+            [2950,3300,3550],
+            [-1150,-1150,0],
+        ],
+        opacity:[
+            [2800,3000, 3050],
+            [1,1,0],
+        ],
+        
+
+       },
+    });
+   
 };
+
